ae3c409492f144368bb9e3a1649d094a2dc636a2
Guadalupe <haniguadalupe@gmail.com>
2025-05-23 11:50:28 +0200

    FIX-ACCECIBILITY

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ContactForm from '../../components/ContactForm/ContactForm';
 
 
+
 export default function Contacto() {
   return (
     <main style={{ padding: '2rem', minHeight: '80vh'}}>
-      <h1 style={{ color: '#F7748D', textAlign: 'center', marginBottom: '1.5rem' }}>
+      <h1 style={{ color: 'rgb(0, 0, 0)', textAlign: 'center', marginBottom: '1.5rem' }}>
         Contáctanos
       </h1>
       <ContactForm />
